86981728e58d0a42518f311f0569f0f9a423c7fa
Brayden Zimmerman <bazman.bz@gmail.com>
2021-07-07 16:20:58 -0400

    mhm

--- bot.ts
+++ bot.ts
@@ -1,70 +1,104 @@
-import Discord from 'discord.js'
+import Discord from 'discord.js';
 import schedule from 'node-schedule';
-import { getRndNum, getRndValue } from './helper';
+import { getRndInt, getRndValue, contains } from './helper';
 
 const client: Discord.Client = new Discord.Client();
 
-var names: string[] = ['love', 'bubs', 'hun', 'honey', 'ava'];
+const cpm: number = 438;
 
-//TODO:: CHANGE THIS TO AVA'S ID
+var names: string[] = ['', 'love', 'bubs', 'hun', 'honey', 'ava'];
+
+//^TODO:: CHANGE THIS TO AVA'S ID
 const avaID: string = '224494447239495682';
 var avaUser: Discord.User;
 
 var goodMorningSchedule: schedule.Job;
 var goodNightSchedule: schedule.Job;
+var dailyMessageSchedule: schedule.Job;
+
+//TODO: Finish dailyMessageMap
+var dailyMessageMap: Map<number, string> = new Map<number, string>( 
+	// [[0, 'message'], [1, 'lmfao']] (template)
+);
 
 client.login('ODYwNjM1NDEyMjg0NzAyNzIw.YN-HMA.ftzSqbckjFvqhS2Bx8fVrwI_0Hs');
 
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user?.tag}!`);
 	avaUser = await client.users.fetch(avaID);
-	
+
 	createSchedules();
 });
 
 
-client.on('message', async(message: Discord.Message) => {
+client.on('message', (message: Discord.Message) => {
 	var user: Discord.User = message.author;
-	var content: string = message.content;
+	var content: string = message.content.trim();
 
 	if(user.id !== avaID) return;
 
 	if(contains(content, 'i love you', 'i love u', 'i luv you', 'i luv u')) {
-		sendAvaMessage(`i love you too, ${getRandomName()}`);
+		sendAvaMessage(`i love you too${getRandomName()}${getRndInt(0, 2) ? '!' : ''}`);
 		return;
 	}
 });
 
-function sendAvaMessage(message: string) {
-	avaUser.send(message);
+async function sendAvaMessage(message: string) {
+
+	if(avaUser.dmChannel == null) {
+		await avaUser.createDM();
+	}
+
+	if(avaUser.dmChannel == null) {
+		console.log("FUCK");
+	}
+
+	avaUser.dmChannel?.startTyping();
+
+	setTimeout(() => {
+		avaUser.dmChannel?.stopTyping();
+		avaUser.send(message);
+	}, determineTypingTime(message));
+	
 }
 
 function getRandomName(): string {
-	return getRndValue(names);
+	var name = getRndValue(names);
+
+	if(name !== '') name = `, ${name}`;
+	return name; 	
+}
+
+function determineTypingTime(message: string): number {
+
+	var cps: number = cpm / 60.0;
+
+	var timeInSec: number = message.length / cps;
+	
+	return timeInSec * 1000;
 }
 
 function createSchedules() {
 	const goodMorningRule = new schedule.RecurrenceRule();
 	const goodNightRule = new schedule.RecurrenceRule();
-
+	const dailyMessageRule = new schedule.RecurrenceRule();
+	
 	goodMorningRule.hour = 8;
 	goodNightRule.hour = 20;
+	dailyMessageRule.dayOfWeek = new schedule.Range(0, 5); dailyMessageRule.hour = [12, 16];
 
 	goodMorningSchedule = schedule.scheduleJob(goodMorningRule, () => {
-		return sendAvaMessage(`good morning, ${getRandomName()}!
-			${Math.floor(getRndNum(0, 2)) ? ' <3' : ''}`);
+		return sendAvaMessage(`good morning, ${getRandomName()}! ${getRndInt(0, 2) ? '<3' : ''}`);
 	});
 
 	goodNightSchedule = schedule.scheduleJob(goodNightRule, () => {
-		return sendAvaMessage(`goodnight, ${getRandomName()}! <3`);
+		return sendAvaMessage(`goodnight, ${getRandomName()}! ${getRndInt(0, 2) ? '<3' : ''}`);
 	});
-}
 
-function contains(str: string, ...phrases: string[]): boolean {
-	
-	for(var x = 0; x < phrases.length; x++) {
-		if(str.includes(phrases[x])) return true;
-	}
-	
-	return false;
+	goodNightSchedule = schedule.scheduleJob(dailyMessageRule, (date: Date) => {
+		var dayOfWeek: number = date.getDay();
+
+		return sendAvaMessage(dailyMessageMap.get(dayOfWeek) as string);
+
+	});
 }
--- helper.ts
+++ helper.ts
@@ -2,11 +2,33 @@ function getRndNum(min: number, max: number): number {
     return (Math.random() * (max - min)) + min;
 }
 
+function getRndInt(min: number, max: number): number {
+	return Math.floor(getRndNum(min, max));
+}
+
 function getRndValue<T>(arr: Array<T>, startingIndex?: number): T {
 
     if(startingIndex === undefined) startingIndex = 0;
 
-    return arr[getRndNum(startingIndex, arr.length)];
+    return arr[Math.floor(getRndNum(startingIndex, arr.length))];
 }
 
-export { getRndNum, getRndValue }
+function contains(str: string, ...phrases: string[]): boolean {
+	
+	for(var x = 0; x < phrases.length; x++) {
+		if(str.includes(phrases[x])) return true;
+	}
+	
+	return false;
+}
+
+function getNumOfChar(str: string, char: string): number {
+	var charNum = 0;
+	for(var x = 0; x < str.length; x++) {
+		if(str.charAt(x) === char) charNum++;
+	}
+
+	return charNum;
+}
+
+export { getRndNum, getRndInt, getRndValue, contains, getNumOfChar }
